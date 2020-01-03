97a5d19dd564c8c3be49bb294711c951547122ab
hadesyi <leesk55@gmail.com>
2020-01-03 14:40:34 +0900

    usd 함수 선언 바꾸기

--- app/statement.js
+++ app/statement.js
@@ -28,20 +28,15 @@ function statement(invoice, plays) {
     let totalAmount = 0;
     let volumeCredits = 0;
     let result = `청구 내역 ( 고객명 : ${invoice.customer})\n`;
-    const format = new Intl.NumberFormat("en-US", {
-        style: "currency",
-        currency: "USD",
-        minimumFractionDigits: 2
-    }).format();
 
     for (let perf of invoice.performance) {
         volumeCredits += volumeCreditsFor(perf);
 
         //청구 내역을 출력한다.
-        result += ` ${playFor(perf).name}: ${format(thisAmount / 100)} (${perf.audience}석)\n`;
+        result += ` ${playFor(perf).name}: ${usd(thisAmount / 100)} (${perf.audience}석)\n`;
         totalAmount += thisAmount;
     }
-    result += `총액: ${format(totalAmount / 100)}\n`;
+    result += `총액: ${usd(totalAmount / 100)}\n`;
     result += `적립 포인트: ${volumeCredits}점\n`;
     return result;
 }
@@ -53,4 +48,11 @@ function volumeCreditsFor(perf) {
     return result;
 }
 
+function usd(aNumber) {
+    return new Intl.NumberFormat("en-US", {
+        style: "currency",
+        currency: "USD",
+        minimumFractionDigits: 2
+    }).format(aNumber/100);
+}
 statement(invoices, plays);
