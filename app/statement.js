65b074299b8eefe2aa2c750c63e5d9249a4fee19
hadesyi <leesk55@gmail.com>
2020-01-03 14:25:05 +0900

    변수 인라인하기

--- app/statement.js
+++ app/statement.js
@@ -1,7 +1,7 @@
 function statement(invoice, plays) {
     let totalAmount = 0;
     let volumeCredits = 0;
-    let result =`청구 내역 ( 고객명 : ${invoice.customer})\n`;
+    let result = `청구 내역 ( 고객명 : ${invoice.customer})\n`;
     const format = new Intl.NumberFormat("en-US", {
         style: "currency",
         currency: "USD",
@@ -9,22 +9,22 @@ function statement(invoice, plays) {
     }).format();
 
     for (let perf of invoice.performance) {
-        const play = playFor(perf);
-        let thisAmount = amountFor(perf, play);
+        let thisAmount = amountFor(perf, playFor(perf));
 
         //포인트를 적립한다.
         volumeCredits += Math.max(perf.audience - 30, 0);
         //희극 관객 5명마다 추가 포인트를 제공한다.
-        if("희극"===play.type) volumeCredits += Math.floor(perf.audience / 5);
+        if ("희극" === playFor(perf).type) volumeCredits += Math.floor(perf.audience / 5);
 
         //청구 내역을 출력한다.
-        result += ` ${play.name}: ${format(thisAmount / 100)} (${perf.audience}석)\n`;
+        result += ` ${playFor(perf).name}: ${format(thisAmount / 100)} (${perf.audience}석)\n`;
         totalAmount += thisAmount;
     }
     result += `총액: ${format(totalAmount / 100)}\n`;
     result += `적립 포인트: ${volumeCredits}점\n`;
     return result;
 }
+
 let invoices = 'app/invoices.json';
 let plays = 'app/plays.json';
 
