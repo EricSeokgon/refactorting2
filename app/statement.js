db9acc95ec01398d61a420a201ef748afe624765
hadesyi <leesk55@gmail.com>
2020-01-03 15:18:27 +0900

    totalA,punt 변수를 인라인후 함수이름 수정

--- app/statement.js
+++ app/statement.js
@@ -29,26 +29,25 @@ function statement(invoice, plays) {
     for (let perf of invoice.performance) {
         result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience}석)\n`;
     }
-    let totalAmount = appleSauce();
-    result += `총액: ${usd(totalAmount)}\n`;
+    result += `총액: ${usd(totalAmount())}\n`;
     result += `적립 포인트: ${(totalVolumeCredits())}점\n`;
     return result;
 }
 
-function appleSauce() {
-    let totalAmount = 0;
+function totalAmount() {
+    let result = 0;
     for (let perf of invoice.performance) {
-        totalAmount += amountFor(perf);
+        result += amountFor(perf);
     }
-    return totalAmount;
+    return result;
 }
 
 function totalVolumeCredits() {
-    let volumeCredits = 0;
+    let result = 0;
     for (let perf of invoice.performance) { // 값 누적 부분을 별도 for문으로 분리
-        volumeCredits += volumeCreditsFor(perf);
+        result += volumeCreditsFor(perf);
     }
-    return volumeCredits;
+    return result;
 }
 
 function volumeCreditsFor(perf) {
@@ -64,4 +63,30 @@ function usd(aNumber) {
         currency: "USD",
         minimumFractionDigits: 2
     }).format(aNumber / 100);
+}
+
+function amountFor(aPerformance) {
+    let result =0;
+    switch (playFor(aPerformance).type) {
+        case "비극":
+            result=40000;
+            if (aPerformance.audience > 30) {
+                result += 1000 * (aPerformance.audience - 30);
+            }
+            break;
+        case "희극":
+            result = 30000;
+            if (aPerformance.audience > 20) {
+                result += 10000 + 500 * (aPerformance.audience - 20);
+            }
+            result += 300 * aPerformance.audience;
+            break;
+        default:
+            throw new Error(`알 수 없는 장르 : ${playFor(aPerformance).type}`);
+    }
+    return result;
+}
+
+function playFor(aPerformance) {
+    return plays[aPerformance.displayId];
 }
