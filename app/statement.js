1e7c29646d8a6634605e78951a6270fac27bd057
hadesyi <leesk55@gmail.com>
2020-01-03 15:49:39 +0900

    매개변수로 전달하는 방식으로 변경

--- app/statement.js
+++ app/statement.js
@@ -28,28 +28,33 @@ function statement(invoice, plays) {
     const statementData = {};
     statementData.customer = invoice.customer;
     statementData.performances = invoice.performances.map(enrichPerformance);
+    statementData.totalAmount = totalAmount(statementData);
+    statementData.totalVolumeCredits = totalVolumeCredits(statementData);
     return renderPlainText(statementData, plays);
 }
 
 function enrichPerformance(aPerformance) {
     const result = Object.assign({}, aPerformance); // 얕은 복사 수행
+    result.play = playFor(result);
+    result.amount = amountFor(result);
+    result.volumeCredits = volumeCreditsFor(result);
     return result;
 }
 
 function renderPlainText(data, invoice, plays) {
     let result = `청구 내역 ( 고객명 : ${data.customer})\n`;
     for (let perf of data.performances) {
-        result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience}석)\n`;
+        result += ` ${perf.play.name}: ${usd(perf.amount)} (${perf.audience}석)\n`;
     }
-    result += `총액: ${usd(totalAmount())}\n`;
-    result += `적립 포인트: ${(totalVolumeCredits())}점\n`;
+    result += `총액: ${usd(data.totalAmount)}\n`;
+    result += `적립 포인트: ${data.totalVolumeCredits}점\n`;
     return result;
 }
 
 function totalAmount() {
     let result = 0;
     for (let perf of data.performances) {
-        result += amountFor(perf);
+        result += perf.amount;
     }
     return result;
 }
@@ -62,10 +67,10 @@ function totalVolumeCredits() {
     return result;
 }
 
-function volumeCreditsFor(perf) {
+function volumeCreditsFor(aPerformance) {
     let result = 0;
-    result += Math.max(perf.audience - 30, 0);
-    if ("희극" === playFor(perf).type) result += Math.floor(perf.audience / 5);
+    result += Math.max(aPerformance.audience - 30, 0);
+    if ("희극" === aPerformance.play.type) result += Math.floor(aPerformance.audience / 5);
     return result;
 }
 
@@ -79,7 +84,7 @@ function usd(aNumber) {
 
 function amountFor(aPerformance) {
     let result = 0;
-    switch (playFor(aPerformance).type) {
+    switch (aPerformance.play.type) {
         case "비극":
             result = 40000;
             if (aPerformance.audience > 30) {
@@ -94,12 +99,13 @@ function amountFor(aPerformance) {
             result += 300 * aPerformance.audience;
             break;
         default:
-            throw new Error(`알 수 없는 장르 : ${playFor(aPerformance).type}`);
+            throw new Error(`알 수 없는 장르 : ${aPerformance.play.type}`);
     }
     return result;
 }
 
 function playFor(aPerformance) {
-    return plays[aPerformance.displayId];
+    return plays[aPerformance.playId];
 }
 
+statement(invoices, plays);
